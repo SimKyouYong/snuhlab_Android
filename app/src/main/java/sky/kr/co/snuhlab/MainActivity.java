03780b93e63632d9994f9d9c60b85b610455345a
Sim KyouYong <SKY@Sim-ui-MacBook-Pro.local>
2017-09-20 15:35:40 +0900

    오류 수정

--- app/src/main/java/sky/kr/co/snuhlab/MainActivity.java
+++ app/src/main/java/sky/kr/co/snuhlab/MainActivity.java
@@ -1,11 +1,15 @@
 package sky.kr.co.snuhlab;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.view.KeyEvent;
 import android.webkit.WebView;
 
 import co.kr.sky.webview.SKYWebview;
@@ -42,4 +46,30 @@ public class MainActivity extends Activity {
 
         }
     };
+    @Override
+    @SuppressLint("NewApi")
+    public boolean onKeyDown(int keyCode, KeyEvent event){
+        if((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()){
+            mWebView.goBack();
+            return true;
+        }else if(keyCode == KeyEvent.KEYCODE_BACK){
+            final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this , AlertDialog.THEME_DEVICE_DEFAULT_LIGHT);
+            builder.setMessage("종료 하시겠습니까?");
+            builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    finish();
+                }
+            });
+            builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                }
+            });
+            final AlertDialog dialog = builder.create();
+            dialog.show();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
 }
